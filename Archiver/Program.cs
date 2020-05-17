df1a2d82ec191964d9a164691e46d14a20970854
denrus99 <denrus99@gmail.com>
2020-05-17 20:26:34 +0500

    It works without save.

--- Archiver/Program.cs
+++ Archiver/Program.cs
@@ -7,20 +7,131 @@ namespace Archiver
 {
     class Program
     {
+        public static Dictionary<char, string> CodeDict = new Dictionary<char, string>();
+
         static void Main(string[] args)
         {
-            var ascii = Encoding.ASCII;
             var bytes = File.ReadAllBytes("testfile.txt");
-            var tree = GetBinaryTree(bytes);
-            EncodeTree(tree, "", "");
-            Console.WriteLine("ShowBinaryTree:");
-            ShowBinaryTree(tree);
-            var encodedString = GetEncodedString(File.ReadAllText("testfile.txt"));
+            GetCodeDict(GetBinaryTree(bytes));
+            var encodedBytes = GetEncodedString(File.ReadAllText("testfile.txt"));
+            // encodedBytes.Insert(0, Convert.ToByte(encodedBytes.Count));
+            // foreach (var d in CodeDict)
+            // {
+            //     var line = d.Key + d.Value + " ";
+            //     foreach (var c in Encoding.ASCII.GetBytes(line))
+            //     {
+            //         encodedBytes.Add(c);
+            //     }
+            // }
+            File.WriteAllBytes("encodedfile.tx", encodedBytes.ToArray());
+            var encodeFileText = File.ReadAllBytes("encodedfile.tx");
+            var encodeBinaryString = StringToBinary(encodeFileText);
+            Console.WriteLine("Bites after encoding:");
+            Console.WriteLine(encodeBinaryString);
+            Console.WriteLine("Здрасьте, я ваш текст: {0}", ExtractFromString(encodeBinaryString));
         }
 
-        public static string GetEncodedString(string text)
+        public static string ExtractFromString(string encodedMsg)
         {
-            return text;
+            var dictForExtract = new Dictionary<string, char>();
+            foreach (var c in CodeDict)
+            {
+                dictForExtract.Add(c.Value, c.Key);
+            }
+
+            var result = new StringBuilder();
+            var partOfMsg = "";
+            var length = 1;
+            while (length < encodedMsg.Length)
+            {
+                partOfMsg = encodedMsg.Substring(0, length);
+                var entries = 0;
+                foreach (var d in dictForExtract)
+                {
+                    if (d.Key.StartsWith(partOfMsg))
+                    {
+                        entries++;
+                    }
+                }
+
+                if (entries == 1)
+                {
+                    result.Append(dictForExtract[encodedMsg.Substring(0, length)]);
+                    encodedMsg = encodedMsg.Remove(0, length);
+                    length = 1;
+                }
+                else if (entries > 1)
+                {
+                    length++;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(partOfMsg, "Строка не является началом ни одного ключа");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static string StringToBinary(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char b in data)
+            {
+                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+            }
+
+            return sb.ToString();
+        }
+
+        public static List<byte> GetEncodedString(string text)
+        {
+            var newBytes = new List<byte>();
+            var encodedString = new StringBuilder();
+            foreach (var elem in text)
+            {
+                // newBytes.Add(Convert.ToByte(CodeDict[elem].PadRight(8,'0'),2));
+                encodedString.Append(CodeDict[elem]);
+                Console.WriteLine(CodeDict[elem]);
+            }
+
+            var zeroCounter = (encodedString.Length % 8 != 0) ? 8 - encodedString.Length % 8 : 0;
+            while (zeroCounter != 0)
+            {
+                encodedString.Append("0");
+                zeroCounter--;
+            }
+
+            for (int i = 0; i < encodedString.Length; i += 8)
+            {
+                newBytes.Add(Convert.ToByte(encodedString.ToString().Substring(i, 8), 2));
+                Console.WriteLine("Bits:{0}, Byte:{1}", encodedString.ToString().Substring(i, 8),
+                    Convert.ToByte(encodedString.ToString().Substring(i, 8), 2));
+            }
+
+            Console.WriteLine(encodedString.ToString());
+            return newBytes;
+        }
+
+        public static void GetCodeDict(Node tree)
+        {
+            if (tree.Left != null)
+            {
+                GetCodeDict(tree.Left);
+            }
+
+            if (tree.Right != null)
+            {
+                GetCodeDict(tree.Right);
+            }
+
+            if (tree.Code != null)
+            {
+                CodeDict.Add(Convert.ToChar(tree.Value), tree.Code);
+                Console.WriteLine("Byte:{0}, Frequency: {1}, Char: {2}, Code: {3}", tree.Value, tree.Frequency,
+                    Convert.ToChar(tree.Value), tree.Code);
+            }
         }
 
         public static void EncodeTree(Node tree, string codeBefore, string currentCode)
@@ -47,8 +158,8 @@ namespace Archiver
                 queue.Add(new Node(key, dictionary[key]));
             }
 
-            Console.WriteLine("ShowQueue");
-            queue.ShowQueue();
+            // Console.WriteLine("ShowQueue");
+            // queue.ShowQueue();
 
             while (queue.GetCount() > 1)
             {
@@ -60,7 +171,9 @@ namespace Archiver
                 queue.Add(node);
             }
 
-            return queue.Remove();
+            var result = queue.Remove();
+            EncodeTree(result, "", "");
+            return result;
         }
 
         public static Dictionary<byte, int> GetFreqDict(byte[] bytes)
@@ -83,6 +196,7 @@ namespace Archiver
             {
                 Console.WriteLine("Freq: {0}, Value: {1}, Char: {2}", dictionary[key], key, Convert.ToChar(key));
             }
+
             return dictionary;
         }
 
