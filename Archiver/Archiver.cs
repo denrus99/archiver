2099940c0600873cece03413b617937e7b0be942
denrus99 <denrus99@gmail.com>
2020-05-19 19:11:55 +0500

    Final commit with comments.

--- Archiver/Archiver.cs
+++ Archiver/Archiver.cs
@@ -8,21 +8,26 @@ namespace Archiver
 {
     public class Archiver
     {
+        //Основной метод для сжатия/кодирования файла
         public static void Encode(string source, string direction)
         {
             var eFile = new EncodedFile();
+            //Получение словаря со значениями символов и кодов для них
             GetCodeDict(GetBinaryTree(File.ReadAllBytes(source)), eFile.codeDictionary);
+            //Получение новых байт для записи их в файл
             eFile.encodedBytes = GetEncodedBytes(File.ReadAllText(source), eFile);
             SaveInFile(eFile, direction);
         }
         
+        //Метод, который выводит коэффициент сжатия
         public static void ShowCompressCoef(string source, string direction)
         {
             var sourceWeight = (double) new FileInfo(source).Length;
             var directionWeight = (double) new FileInfo(direction).Length;
             Console.WriteLine("Коэффициент сжатия составляет: {0}", sourceWeight / directionWeight);
         }
-
+        
+        //Метод записывающий итоговый массив закодированных байт в файл
         static void SaveInFile(EncodedFile eFile, string path)
         {
             var bf = new BinaryFormatter();
@@ -33,6 +38,7 @@ namespace Archiver
             }
         }
 
+        //Получает объект, в котором хранится массив закодированных байт и словарь для декодирования
         static EncodedFile ExtractFromFile(string path)
         {
             var bf = new BinaryFormatter();
@@ -43,17 +49,20 @@ namespace Archiver
             }
         }
 
+        //Основной метод для декодирования информации из файла
         public static void Decode(string direction, string newDirection)
         {
             var eFile = ExtractFromFile(direction);
             var dictionaryForDecode = new Dictionary<string, char>();
             var binaryStr = StringToBinary(eFile.encodedBytes.ToArray());
+            //Вычитаем лишние нули из полученной строки
             binaryStr = binaryStr.Substring(0, binaryStr.Length - eFile.countOfZeros);
             foreach (var c in eFile.codeDictionary)
             {
                 dictionaryForDecode.Add(c.Value, c.Key);
             }
 
+            //Декодирование исходного сообщения по словарю
             var result = new StringBuilder();
             var partOfMsg = "";
             var length = 1;
@@ -79,15 +88,13 @@ namespace Archiver
                 {
                     length++;
                 }
-                else
-                {
-                    throw new ArgumentOutOfRangeException(partOfMsg, "Строка не является началом ни одного ключа");
-                }
             }
             File.WriteAllText(newDirection, result.ToString());
             Console.WriteLine("Файл разархивирован в " + newDirection);
         }
 
+        
+        //Вспомогательный метод для получения строки, состоящей из "0" и "1".
         public static string StringToBinary(byte[] data)
         {
             StringBuilder sb = new StringBuilder();
@@ -100,6 +107,8 @@ namespace Archiver
             return sb.ToString();
         }
 
+        
+        //Метод, кодирующий текст по составленному словарю
         public static List<byte> GetEncodedBytes(string text, EncodedFile eFile)
         {
             var newBytes = new List<byte>();
@@ -109,6 +118,7 @@ namespace Archiver
                 encodedString.Append(eFile.codeDictionary[elem]);
             }
 
+            //Добавление нулей в конце для того, чтобы длина строки была кратна 8
             var zeroCounter = (encodedString.Length % 8 != 0) ? 8 - encodedString.Length % 8 : 0;
             eFile.countOfZeros = zeroCounter;
             while (zeroCounter != 0)
@@ -124,6 +134,7 @@ namespace Archiver
             return newBytes;
         }
 
+        //Получение словаря <символ, код> из дерева Хаффмана
         public static void GetCodeDict(Node tree, Dictionary<char, string> dictionary)
         {
             if (tree.Left != null)
@@ -144,6 +155,7 @@ namespace Archiver
             }
         }
 
+        //Добавление кодов в дерево Хаффмана
         public static void EncodeTree(Node tree, string codeBefore, string currentCode)
         {
             if (tree.Right == null && tree.Left == null)
@@ -159,9 +171,12 @@ namespace Archiver
             }
         }
 
+        //Составление дерева Хаффмана
         public static Node GetBinaryTree(byte[] bytes)
         {
+            //Получаем словарь <символ, частота>
             var dictionary = GetFreqDict(bytes);
+            //Создаем приоритетную очередь, в которое самым приоритетным считается символ с самой маленькой частотой
             var queue = new PriorityQueue();
             foreach (var key in dictionary.Keys)
             {
@@ -170,19 +185,23 @@ namespace Archiver
 
             while (queue.GetCount() > 1)
             {
+                //Берём два первых узла из очереди и соединяем их в новый с суммарной частотой его потомков
                 var firstLeaf = queue.Remove();
                 var secondLeaf = queue.Remove();
                 var node = new Node();
                 node.Add(firstLeaf);
                 node.Add(secondLeaf);
+                //Кладём, получившийся узел в очередь 
                 queue.Add(node);
             }
 
             var result = queue.Remove();
+            //Получаем код для каждого символа в дереве
             EncodeTree(result, "", "");
             return result;
         }
 
+        //Метод, возвращающий словарь <символ, частота>
         public static Dictionary<byte, int> GetFreqDict(byte[] bytes)
         {
             var dictionary = new Dictionary<byte, int>();
@@ -200,24 +219,9 @@ namespace Archiver
             Console.WriteLine("Таблица символов и кодов для них:");
             return dictionary;
         }
-
-        static void ShowBinaryTree(Node root)
-        {
-            if (root.Left != null)
-            {
-                ShowBinaryTree(root.Left);
-            }
-
-            if (root.Right != null)
-            {
-                ShowBinaryTree(root.Right);
-            }
-
-            if (root.Code != null)
-                Console.WriteLine("Byte:{0}, Frequency: {1}, Char: {2}, Code: {3}", root.Value, root.Frequency,
-                    Convert.ToChar(root.Value), root.Code);
-        }
     }
+  
+    //Класс, реализующий бинарное дерево
     public class Node
     {
         public Node Left { get; set; }
@@ -256,6 +260,7 @@ namespace Archiver
         }
     }
 
+    //Класс, реализующий приоритетную очередь
     public class PriorityQueue
     {
         private List<Node> data;
@@ -303,6 +308,7 @@ namespace Archiver
         }
     }
     
+    //Класс, для последующего сохранения в файл
     [Serializable]
     public class EncodedFile
     {
